ed0d62172dbb3e1a25382d993e0b9c1853c14c7c
Devraj326 <141002491+Devraj326@users.noreply.github.com>
2023-11-18 18:49:03 +0530

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-export default defineConfig(() => {
-  return {
-    build: {
-      outDir: 'build',
-    },
-    plugins: [react()],
-    server:{
-      port:3000
-    },
-  };
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server:{
+    port:3000
+  },
 });
